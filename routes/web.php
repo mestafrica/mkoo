2f1d7398b3fde8d2a8b71c406130f9c609732c67
Francis Addai <me@faddai.com>
2017-11-16 03:09:41 +0000

    Add password reset functionality

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ Route::get('/', ['as' => 'login', 'uses' => 'Auth\PasswordLoginController@showLo
 Route::get('login', ['as' => 'login', 'uses' => 'Auth\PasswordLoginController@showLoginForm']);
 Route::post('login', ['as' => 'login', 'uses' => 'Auth\PasswordLoginController@login']);
 Route::post('logout', ['as' => 'logout', 'uses' => 'Auth\PasswordLoginController@logout']);
+Route::post('password/email', ['as' => 'password.email', 'uses' => 'Auth\ForgotPasswordController@sendResetLinkEmail']);
+Route::get('password/reset', ['as' => 'password.request', 'uses' => 'Auth\ForgotPasswordController@showLinkRequestForm']);
+Route::post('password/reset', ['uses' => 'Auth\ResetPasswordController@reset']);
+Route::get('password/reset/{token}', ['as' => 'password.reset', 'uses' => 'Auth\ResetPasswordController@showResetForm']);
 
 Route::group(['prefix' => 'auth'], function(){
     Route::get('google', ['as' => 'auth.google', 'uses' => 'Auth\GoogleAuthController@redirectToProvider']);

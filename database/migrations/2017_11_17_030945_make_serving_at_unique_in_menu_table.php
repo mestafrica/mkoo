a70a03555f69e19582ee0197d11779d3553cb424
eddymens <kwasiamantin@gmail.com>
2017-11-17 03:15:41 +0000

    add migration to make serving field unqiue

---
+++ database/migrations/2017_11_17_030945_make_serving_at_unique_in_menu_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MakeServingAtUniqueInMenuTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('menus', function (Blueprint $table) {
+            $table->string('name')->unique;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('menus', function (Blueprint $table) {
+            //
+        });
+    }
+}

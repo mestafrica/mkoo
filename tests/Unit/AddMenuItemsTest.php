f8973662dca39af87f6747c45aed10a13a35ae5c
eddymens <kwasiamantin@gmail.com>
2017-11-20 18:16:32 +0000

    adjust test base on changes

--- tests/Unit/AddMenuItemsTest.php
+++ tests/Unit/AddMenuItemsTest.php
@@ -4,7 +4,6 @@ namespace Tests\Unit;
 
 use Carbon\Carbon;
 use Tests\TestCase;
-use App\Entities\MenuItem;
 use App\Jobs\AddMenuItems;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
@@ -15,8 +14,14 @@ class AddMenuItemsTest extends TestCase
     public function test_can_add_menu_items()
     {
 
-        $weeklySelection = factory(MenuItem::class)->make()->toArray();
+        $weeklySelection = [];
+        $days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
 
+        foreach ($days as $day) {
+            $weeklySelection[$day]['lunch'] = [rand(1, 40),rand(1, 40)];
+            $weeklySelection[$day]['dinner'] = [rand(1, 40),rand(1, 40)];
+        }
+        
         $weeklySelection['menu_id'] = 1;
 
         $response = dispatch_now(new AddMenuItems($weeklySelection));

562f639141635799883249a335271a0191cd7eda
Solomon Appier-Sign <braasig@gmail.com>
2017-11-22 10:13:24 +0000

    name mutator added to the Item model

--- app/Entities/Item.php
+++ app/Entities/Item.php
@@ -6,8 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Item extends Model
 {
-    //
     protected $fillable = [
         'name', 'description', 'created_by'
     ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function meals()
+    {
+        return $this->belongsToMany(Meal::class)->withTimestamps();
+    }
+
+    /**
+     * @param $value
+     * @return string
+     */
+    public function setNameAttribute($value)
+    {
+        return $this->attributes['name'] = ucwords($value);
+    }
 }

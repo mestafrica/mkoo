1903318de98f9d3f5532299ba722289af44e06cc
Solomon Appier-Sign <braasig@gmail.com>
2017-11-22 10:11:47 +0000

    reversed changes made to the Item model

--- app/Entities/Item.php
+++ app/Entities/Item.php
@@ -6,25 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Item extends Model
 {
-    //
     protected $fillable = [
         'name', 'description', 'created_by'
     ];
-
-    /**
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
-     */
-    public function meals()
-    {
-        return $this->belongsToMany(Meal::class)->withTimestamps();
-    }
-
-    /**
-     * @param $value
-     * @return string
-     */
-    public function setNameAttribute($value)
-    {
-        return $this->attributes['name'] = ucwords($value);
-    }
 }

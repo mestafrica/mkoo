a1adf3641de6ceea2d0a0ec02ae19d6fe8f06f63
eddymens <kwasiamantin@gmail.com>
2017-11-23 03:40:53 +0000

    adjust test

--- tests/Unit/AddMenuJobTest.php
+++ tests/Unit/AddMenuJobTest.php
@@ -14,29 +14,17 @@ class AddMenuJobTest extends TestCase
 
     public function test_can_add_menu()
     {
+        $today = Carbon::now()->format('l');
         $this->setRequestUser()
             ->merge(factory(Menu::class)->make()->toArray());
         
+        if (!collect(config('allowed_dates')['menu'])->contains($today)) {
+            $this->expectException(\Exception::class);
+        }
+        
         $menu = dispatch_now(new AddMenuJob($this->request));
-
         self::assertInstanceOf(Menu::class, $menu);
         self::assertNotNull($menu->created_by);
         self::assertNotNull($menu->serving_at);
-    }
-
-    public function test_can_select_menu()
-    {
-
-        $this->setRequestUser()
-            ->merge(
-                factory(Menu::class)->make()->toArray()
-            );
-
-        $menu = new Menu(['id'=>1, 'created_by'=>1]);
-        $response = dispatch_now(new AddMenuJob($this->request, $menu));
-
-        self::assertInstanceOf(Menu::class, $response);
-        self::assertNotNull($response->created_by);
-        self::assertNotNull($response->serving_at);
     }
 }

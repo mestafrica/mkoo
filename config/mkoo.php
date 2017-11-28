534ff685eb12b884cad24826ae216326d22add43
Francis Addai <me@faddai.com>
2017-11-28 11:21:38 +0000

    Add all configs in config/mkoo.php

--- config/mkoo.php
+++ config/mkoo.php
@@ -16,4 +16,16 @@ return [
         'gmail.com',
     ],
 
+    /*
+     * Days on which Kitchen staff creates menu
+     */
+
+    'days_for_menu_creation' => ['Monday', 'Tuesday'],
+
+    /*
+     * Days on which orders can be submitted
+     */
+
+    'days_for_order_placement' => ['Wednesday', 'Thursday'],
+
 ];
